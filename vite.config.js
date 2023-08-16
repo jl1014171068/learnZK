2dd40249da9164945bb5ff9cdf39319374299932
jianglong <1146937621@zto.com>
2023-08-16 13:44:57 +0800

    feat: 提交

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicPath: process.env.NODE_ENV === 'production' ? '/learnZK/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
